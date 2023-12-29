aefd524077f959ff96158efe0d70f0bc619355ae
jorgenetoo <jorgenetoo@yahoo.com.br>
2023-12-29 12:22:48 -0300

    último commit

--- index.js
+++ index.js
@@ -1,8 +1,99 @@
+//import express from 'express'
+
 const express = require('express')
-const app = express()
-const port = 3000
+
 const uuid = require('uuid')
+
+import cors from 'cors'
+//const cors = require('cors')
+
+const port = 3001;
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const users = []
+
+const checkUserId = (request, response, next) => {
+
+    const { id } = request.params 
+
+    const index = users.findIndex( user => user.id === id)
+
+    if(index < 0){
+        return response.status(404).json({ error: "user not found"})
+    }
+
+    request.userIndex = index
+    request.userId = id
+
+    next()
+}
+
+
+app.get('/users', (request, response) => {
+
+    return response.json(users)
+
+})
+
+
+app.post('/users', (request, response) => {
+
+    const { name, age } = request.body
+
+    const user = { id: uuid.v4(), name, age }
+
+    users.push(user)
+
+    return response.status(201).json(user)
+
+})
+
+app.put('/users/:id', checkUserId , (request, response) => {
+    
+
+    const {name, age} = request.body
+    const id = request.userId
+    const index = request.userIndex
+
+    const updateUser = { id, name, age }
+
+    users[index] = updateUser
+
+    return response.json(updateUser)
+
+})
+
+
+app.delete('/users/:id', checkUserId , (request, response) => {
+
+    const index = request.userIndex    
+    users.splice(index, 1)
+
+    return response.status(204).json()
+
+})
+
+
+
+app.listen(port, () => {
+    console.log(`🚀 server started on port ${port}`)
+})
+
+/*const express = require("express")
+
+const uuid = require('uuid')
+
+const cors = require('cors')
+
+const app = express()
+
+
+import cors from 'cors'
+const port = 3001
 app.use(express.json())
+app.use(cors())
 
 const orders = []
 
@@ -90,4 +181,4 @@ app.patch('/orders/:id', (request, response) => {
 
 app.listen(port, () => {
     console.log(`🚀Server Started on Port ${port}🚀`)
-})
+})*/
